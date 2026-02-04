8dc996a104eebe91b8f2daea871d80885160cef1
Daniel Robbins <daniel@macmate.app>
2026-02-04 11:13:05 +0800

    Release 1.0.10

--- Casks/macmate.rb
+++ Casks/macmate.rb
@@ -1,6 +1,6 @@
 cask "macmate" do
-  version "1.0.9"
-  sha256 "84de069c95ea160cff0a1044176753360f0fd402c826b668537714c348809ffb"
+  version "1.0.10"
+  sha256 "5f8ec333df545b0a822b2db6a5ebbb4e4cf15f43e186a12e51c1ed0563a342ce"
 
   url "https://release.macmate.app/releases/MacMate-#{version}.dmg"
   name "MacMate"
